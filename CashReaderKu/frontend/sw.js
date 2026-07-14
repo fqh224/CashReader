6999a8350be3b653091ad8127190f2c13f66eced
fqh224 <faqihsunay01@gmail.com>
2026-07-14 11:52:43 +0900

    revisi sw.js

--- CashReaderKu/frontend/sw.js
+++ CashReaderKu/frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cashreader-v3'; // Naikkan versinya!
+const CACHE_NAME = 'cashreader-v1'; // Naikkan versinya!
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
